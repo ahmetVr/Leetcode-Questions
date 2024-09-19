ff59d30a17c110fd0c6fa7d34fbc9732f023ac21
Ahmet Var <104149041+ahmetVr@users.noreply.github.com>
2024-09-19 23:12:47 +0300

    3Sum soru açıklaması güncellendi

--- Algorithm/Java/3Sum/SumThree.java
+++ Algorithm/Java/3Sum/SumThree.java
@@ -2,6 +2,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+Given an integer array nums, return all the triplets [nums[i],
+nums[j], nums[k]] such that i != j, i != k, and j != k,
+and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets. 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+*/
+
 public class SumThree {
      public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
