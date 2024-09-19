fc055513d21ce3610ccf9cf18deff46eda666f32
Ahmet Var <104149041+ahmetVr@users.noreply.github.com>
2024-09-19 23:10:01 +0300

    Update MaximumSubarray.java
    Explaining added

--- Algorithm/Java/MaximumSubarray/MaximumSubarray.java
+++ Algorithm/Java/MaximumSubarray/MaximumSubarray.java
@@ -1,3 +1,15 @@
+ /*
+     * Given an integer array nums, find the subarray
+     * with the largest sum, and return its sum.
+     * 
+     * Constraints
+     * 1 <= nums.length <= 105
+     * -104 <= nums[i] <= 104
+     * 
+     * Follow up: If you have figured out the O(n) solution,
+     * try coding another solution using the divide and conquer approach, which is more subtle.
+     */
+
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
